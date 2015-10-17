3a2167d8d86ccde9e112cb81d817bd17f2b50e0b
Sean Hutchinson <sean.hutchinson93@gmail.com>
2015-10-17 14:38:17 -0400

    Added main to mobile-device

--- mobile-device/src/main/java/com/hutch/keyboard/mobile_device/MobileDevice.java
+++ mobile-device/src/main/java/com/hutch/keyboard/mobile_device/MobileDevice.java
@@ -1,9 +1,28 @@
 package com.hutch.keyboard.mobile_device;
 
+import java.io.IOException;
+
+import com.hutch.keyboard.autocomplete.Autocomplete;
+
 public class MobileDevice {
-
+	private Autocomplete autocomplete;
+	
+	private MobileDevice() {
+		autocomplete = new Autocomplete();
+		System.out.println("Train -> \"The third thing that I need to tell you is that this thing does not think thoroughly.\"");
+		autocomplete.addSentence("The third thing that I need to tell you is that this thing does not think thoroughly.");
+		System.out.println("getWords(\"thi\"): \t" + autocomplete.getWords("thi").toString());
+		System.out.println("getWords(\"nee\"): \t" + autocomplete.getWords("nee").toString());
+		System.out.println("getWords(\"th\"): \t" + autocomplete.getWords("th").toString());
+		System.out.println("Press <Enter> to exit.");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// exit
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MobileDevice();
 	}
 }
